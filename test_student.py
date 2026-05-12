437aa926617329a4d778f4fa9d3e2538944e74ef
smanog <smanog@calpoly.edu>
2026-05-11 20:13:11 -0700

    test creation

--- test_student.py
+++ test_student.py
@@ -1,5 +1,5 @@
 import unittest
-from proj3 import huffman_encoding, heapify_up
+from proj3 import huffman_encoding, heapify_up, heapify_down
 
 class TestProj3(unittest.TestCase):
 
@@ -7,6 +7,11 @@ class TestProj3(unittest.TestCase):
         heap = [5, 10, 8, 20, 15, 9, 1]
         result = heapify_up(heap, 6)
         self.assertEqual(result, [1, 10, 5, 20, 15, 9, 8])
+
+    def test_heapify_down(self):
+        heap = [9, 3, 5]
+        result = heapify_down(heap, 0)
+        self.assertEqual(result, [3, 9, 5])
 
 if __name__ == "__main__":
     unittest.main()
