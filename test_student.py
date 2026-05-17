53ed836b42ef9f0e69d265764940186de37533c9
smanog <smanog@calpoly.edu>
2026-05-17 16:52:20 -0700

    more tests

--- test_student.py
+++ test_student.py
@@ -1,5 +1,5 @@
 import unittest
-from proj3 import huffman_encoding, heapify_up, heapify_down
+from proj3 import huffman_encoding, heapify_up, heapify_down, Node, MinHeap, insert
 
 class TestProj3(unittest.TestCase):
 
@@ -12,6 +12,11 @@ class TestProj3(unittest.TestCase):
         heap = [9, 3, 5]
         result = heapify_down(heap, 0)
         self.assertEqual(result, [3, 9, 5])
+
+    def test_insert(self):
+        heap = MinHeap([Node(1, 'A', Node(3, 'F', Node(5, 'S')))])
+        result = insert(heap, Node(20, 'Z'))
+        self.assertEqual(result, MinHeap([Node(1, 'A', Node(3, 'F', Node(5, 'S', Node(20, 'Z'))))]))
 
 if __name__ == "__main__":
     unittest.main()
