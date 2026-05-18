be6ae9f9c08f0b0910161c610aa09656dc389a45
smanog <smanog@calpoly.edu>
2026-05-18 11:58:46 -0700

    more tests

--- test_student.py
+++ test_student.py
@@ -1,11 +1,11 @@
 import unittest
-from proj3 import huffman_encoding, heapify_up, heapify_down, Node, MinHeap, insert
+from proj3 import huffman_encoding, heapify_up, heapify_down, Node, MinHeap, insert, extract_min
 
 class TestProj3(unittest.TestCase):
 
     def test_heapify_up(self):
-        heap = MinHeap([Node(1, 'A', Node(3, 'F', Node(5, 'S')))])
-        result = heapify_up(heap, 6)
+        heap = MinHeap([Node(1, 'A', Node(3, 'F', Node(5, 'S'))), Node(7, 'z')])
+        result = heapify_up(heap, 0)
         self.assertEqual(result, [1, 10, 5, 20, 15, 9, 8])
 
     def test_heapify_down(self):
@@ -17,6 +17,11 @@ class TestProj3(unittest.TestCase):
         heap = MinHeap([Node(1, 'A', Node(3, 'F', Node(5, 'S')))])
         result = insert(heap, Node(20, 'Z'))
         self.assertEqual(result, MinHeap([Node(1, 'A', Node(3, 'F', Node(5, 'S'), Node(20, 'Z')))]))
+
+    def test_extract_min(self):
+        heap = MinHeap([Node(1, 'A', Node(3, 'F', Node(5, 'S')))])
+        result = extract_min(heap)
+        self.assertEqual(result, Node(1, 'A', Node(3, 'F', Node(5, 'S'))))
 
 if __name__ == "__main__":
     unittest.main()
